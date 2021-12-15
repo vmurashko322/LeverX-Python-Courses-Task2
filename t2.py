616225cc4aa6013c452c6853a35523a8dcafc17f
vmurashko322 <v.murashko999@gmail.com>
2021-12-16 00:53:13 +0300

    first version

--- t2.py
+++ t2.py
@@ -3,37 +3,39 @@ import functools
 
 @functools.total_ordering
 class Version:
-    _suffixes = {
-        "alpha": "1",
-        "beta": "2",
-        "b": ".2",
-        "rc": "3",
-        "-": ".",
-        "release": ".5"
-    }
-
     def __init__(self, version):
         self.version = version
 
-    @functools.cached_property
-    def as_tuple(self):
+    def process_version(self):
         new_version = self.version
-        flag = False
-
-        for i in self._suffixes:
-            if new_version.find(i) != -1:
-                flag = True
-                new_version = new_version.replace(i, self._suffixes[i])
-
-        if not flag:
-            new_version += self._suffixes.get("release")
+        new_dict = {
+            'alpha': "1",
+            "beta": "2",
+            "b": ".2",
+            "rc": "3",
+            "-": "."
+        }
+        for i in new_dict:
+            new_version = new_version.replace(i, new_dict[i])
         return tuple(int(i) for i in new_version.split("."))
 
     def __eq__(self, other):
-        return self.as_tuple is other.as_tuple
+        return self.process_version() == other.as_tuple()
+
+    def __ne__(self, other):
+        return not self.process_version() == other.process_version()
 
     def __lt__(self, other):
-        return self.as_tuple < other.as_tuple
+        first = self.process_version()
+        second = other.process_version()
+        if first[0] == second[0] and first[1] == second[1] and first[2] == second[2]:
+            l1 = len(self.process_version())
+            l2 = len(other.process_version())
+            if l2 < l1:
+                return True
+            elif l2 > l1:
+                return False
+        return self.process_version() < other.process_version()
 
 
 def main():
@@ -44,8 +46,8 @@ def main():
         ("1.1.0-alpha", "1.2.0-alpha.1"),
         ("1.0.1b", "1.0.10-alpha.beta"),
         ("1.0.0-rc.1", "1.0.0"),
-        ("1.0.0-alpha.beta", "1.0.0-beta"),
-        ("1.0.0-beta.11", "1.0.0-rc.1")
+        # ("1.0.0-alpha.beta", "1.0.0-beta"),
+        # ("1.0.0-beta.11", "1.0.0-rc.1")
     ]
 
     for version_1, version_2 in to_test:
